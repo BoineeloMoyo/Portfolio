29b9109ae1a3d3fa04c4efd9b285e5588289a64c
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2024-11-03 15:07:22 +0100

    scroll reveal about content

--- script.js
+++ script.js
@@ -51,4 +51,6 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-info, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .proficiency-container, portfolio-box. .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-img, .proficiency-container, .portfolio-box .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-info h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-info p, .about-content', { origin: 'right' });
