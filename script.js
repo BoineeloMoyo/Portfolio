4ef9116983da226201c455938086181d5247c231
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2024-11-09 10:38:23 +0100

    trial 2

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const circles = document.querySelectorAll('.circle');
 circles.forEach(elem=> {
     var dots = elem.getAttribute('data-dots');
     var marked = elem.getAttribute('data-percent');
-    var percent = Math.floor(dots*marked /100);
+    var percent = Math.floor(dots*marked/100);
     var points = "";
     var rotate = 360 / dots;
 
