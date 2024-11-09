100aa88fbdae3b23c974711777dc89fd865e738d
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2024-11-09 10:36:52 +0100

    animation delay

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ circles.forEach(elem=> {
     var points = "";
     var rotate = 360 / dots;
 
-    for (let i = 0; i < dots; i++) {
+    for(let i = 0; i < dots; i++) {
         points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
     }
     elem.innerHTML = points;
