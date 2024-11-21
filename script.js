ccca1c64c5cc36c8d919ac5279dc3147baac91b6
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2024-11-21 10:37:18 +0100

    removed old circle js script

--- script.js
+++ script.js
@@ -54,28 +54,6 @@ ScrollReveal().reveal('.home-img, .proficiency-container, .portfolio-box .contac
 ScrollReveal().reveal('.home-info h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-info p, .about-content', { origin: 'right' });
 
-/* =======================  Professional pie charts  ================================ */
-
-const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=> {
-    var dots = elem.getAttribute('data-dots');
-    var marked = elem.getAttribute('data-percent');
-    var percent = Math.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots;
-
-    for(let i = 0; i < dots; i++) {
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
-    }
-    elem.innerHTML = points;
-    const pointsMarked = elem.querySelectorAll('.points');
-    for (let i =  0; i<percent; i++) {
-        pointsMarked[i].classList.add('pointsMarked');
-
-        
-    }
-
-})
 
 
 
