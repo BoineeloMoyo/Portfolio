e88d68af27be6fcdbbd9f55ec4e73de2e6b60d2e
Boineelo Moyo <82944675+BoineeloMoyo@users.noreply.github.com>
2025-04-12 10:20:14 +0200

    add php for contact form

---
+++ php/form-process.php
@@ -0,0 +1,76 @@
+<?php
+
+$errorMSG = "";
+
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+// TELEPHONE
+if (empty($_POST["telephone"])) {
+    $errorMSG .= "Telephone number is required ";
+} else {
+    $telephone = $_POST["telephone"];
+}
+
+// SUBJECT
+if (empty($_POST["subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $subject = $_POST["subject"];
+}
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+
+$EmailTo = "boimoyo@gmail.com";
+$Subject = "Enquiry From Boineelo Website";
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Telephone: ";
+$Body .= $telephone;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+
+?>
